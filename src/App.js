34bd007d5b83d245d41a46c7cdff366e375576df
alivane <vaneg643@gmail.com>
2020-06-14 18:16:12 -0400

    add new file for routes link

--- src/App.js
+++ src/App.js
@@ -1,22 +1,4 @@
 import React from 'react';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Home from './pages/Home';
-import AboutUs from './pages/AboutUs';
-import Recovery from './pages/Recovery';
-import AssembleMenu from './pages/AssembleMenu';
-import BranchOffice from './pages/BranchOffice';
-import Contact from './pages/Contact';
-import Schedule from './pages/Schedule';
-import Work from './pages/Work';
-import SelectionMenu from './pages/SelectionMenu';
-import Corporate from './pages/Corporate';
-import MenuSemanal from './pages/MenuSemanal';
-import EnvioForm from './pages/Envio';
-import MenuMaintainer from './pages/MenuMaintainer';
-import PedidosMaintainer from './pages/PedidosMaintainer';
-
-
 import './App.css';
 import {
   BrowserRouter as Router,
@@ -24,62 +6,22 @@ import {
   Route
 } from 'react-router-dom';
 
-function App() {
+import { routes } from './routes';
+
+const App = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/">
-          <Login />
-        </Route>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/register">
-          <Register />
-        </Route>
-        <Route path="/recovery">
-          <Recovery />
-        </Route>
-        <Route path="/home">
-          <Home />
-        </Route>
-        <Route path="/aboutus">
-          <AboutUs />
-        </Route>
-        <Route path="/assemble">
-          <AssembleMenu />
-        </Route>
-        <Route path="/branch">
-          <BranchOffice />
-        </Route>
-        <Route path="/contact">
-          <Contact />
-        </Route>
-        <Route path="/schedule">
-          <Schedule />
-        </Route>
-        <Route path="/work">
-          <Work />
-        </Route>
-        <Route path="/selection">
-          <SelectionMenu />
-        </Route>
-        <Route path="/corporate">
-          <Corporate />
-        </Route>
-        <Route path="/menusemanal">
-          <MenuSemanal />
-        </Route>
-
-        <Route path="/envio">
-          <EnvioForm />
-        </Route>
-        <Route path="/menu-maintainer">
-          <MenuMaintainer />
-        </Route>
-        <Route path="/pedidos-maintainer">
-          <PedidosMaintainer />
-        </Route>
+        {
+          routes.map((route) => (
+            <Route
+              exact
+              key={route.path}
+              path={route.path}
+              component={route.component}
+            />
+          ))
+        }
       </Switch>
     </Router>
   );
---
+++ src/routes.jsx
@@ -0,0 +1,92 @@
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Home from './pages/Home';
+import AboutUs from './pages/AboutUs';
+import Recovery from './pages/Recovery';
+import AssembleMenu from './pages/AssembleMenu';
+import BranchOffice from './pages/BranchOffice';
+import Contact from './pages/Contact';
+import Schedule from './pages/Schedule';
+import Work from './pages/Work';
+import SelectionMenu from './pages/SelectionMenu';
+import Corporate from './pages/Corporate';
+import MenuSemanal from './pages/MenuSemanal';
+import EnvioForm from './pages/Envio';
+import MenuMaintainer from './pages/MenuMaintainer';
+import PedidosMaintainer from './pages/PedidosMaintainer';
+
+
+export const routes = [
+  {
+    path: '/',
+    component: Login
+  },
+  {
+    path: '/login',
+    component: Login
+  },
+  {
+    path: '/register',
+    component: Register
+  },
+  {
+    path: '/recovery',
+    component: Recovery
+  },
+  {
+    path: '/home',
+    component: Home
+  },
+  {
+    path: '/aboutus',
+    component: AboutUs
+  },
+  {
+    path: '/assemble',
+    component: AssembleMenu
+  },
+  {
+    path: '/branch',
+    component: BranchOffice
+  },
+  {
+    path: '/contact',
+    component: Contact
+  },
+  {
+    path: '/contact',
+    component: Contact
+  },
+  {
+    path: '/schedule',
+    component: Schedule
+  },
+  {
+    path: '/work',
+    component: Work
+  },
+  {
+    path: '/selection',
+    component: SelectionMenu
+  },
+  {
+    path: '/corporate',
+    component: Corporate
+  },
+  {
+    path: '/menusemanal',
+    component: MenuSemanal
+  },
+  {
+    path: '/envio',
+    component: EnvioForm
+  },
+  {
+    path: '/menu-maintainer',
+    component: MenuMaintainer
+  },
+  {
+    path: '/pedidos-maintainer',
+    component: PedidosMaintainer
+  }
+]
