2050944859871b48865165f211ee2635151d86bb
alivane <vaneg643@gmail.com>
2020-06-14 23:38:57 -0400

    fix styles menu semanal

--- src/containers/MenuSemanal/MenuSemanalForm.jsx
+++ src/containers/MenuSemanal/MenuSemanalForm.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { makeStyles, withStyles } from '@material-ui/core/styles';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
@@ -8,16 +7,18 @@ import Box from '@material-ui/core/Box';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import ButtonBase from '@material-ui/core/ButtonBase';
-import {SelectionOption} from '../SelectionMenu/SelectionMenuStyle';
-import {menuStyles} from './ButtonStyle';
+import { SelectionOption } from '../SelectionMenu/SelectionMenuStyle';
+import { menuStyles } from './ButtonStyle';
 import Button from '@material-ui/core/Button';
 import PlaylistAddCheckIcon from '@material-ui/icons/PlaylistAddCheck';
-function TabPanel(props) {
+
+
+const TabPanel = (props) => {
   const { children, value, index, ...other } = props;
 
   return (
     <div
-      role="tabpanel"
+      role='tabpanel'
       hidden={value !== index}
       id={`vertical-tabpanel-${index}`}
       aria-labelledby={`vertical-tab-${index}`}
@@ -38,190 +39,121 @@ TabPanel.propTypes = {
   value: PropTypes.any.isRequired,
 };
 
-function a11yProps(index) {
+const a11yProps = (index) => {
   return {
     id: `vertical-tab-${index}`,
     'aria-controls': `vertical-tabpanel-${index}`,
   };
 }
 
-export default function VerticalTabs() {
+const VerticalTabs = (props) => {
   const selectionop = SelectionOption();
   const menu = menuStyles();
   const [value, setValue] = React.useState(0);
-
+  const { dataMenu } = props
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
   return (
     <>
-      <div className="tab">
-    <div className={selectionop.root}>
-      <Tabs
-        orientation="vertical"
-        variant="scrollable"
-        value={value}
-        onChange={handleChange}
-        aria-label="Vertical tabs example"
-        className={selectionop.tabs}
-      >
+      <div className='tab'>
+        <div className={selectionop.root}>
+          <Tabs
+            orientation='vertical'
+            variant='scrollable'
+            value={value}
+            onChange={handleChange}
+            aria-label='Vertical tabs example'
+            className={selectionop.tabs}
+          >
+            {
+              dataMenu &&
+              dataMenu.map((item, index) => (
+                <Tab key={index} label={item.menu} {...a11yProps({ index })} />
+              ))
+            }
+
+          </Tabs>
+          <div className='tab1'>
+            {
+              dataMenu &&
+              dataMenu.map((item, index) => (
+                <TabPanel key={index} value={value} index={index}>
+                  <Grid container>
+                    <Grid item xs={12}>
+                      <Paper className={selectionop.paper}>
+                        <Grid container spacing={2}>
+                          <Grid item>
+                            <ButtonBase className={selectionop.image}>
+                              <img className={selectionop.img} alt='complex' src={item.image} />
+                            </ButtonBase>
+                          </Grid>
+                          <Grid item xs={12} sm container>
+                            <Grid item xs container direction='column' spacing={2}>
+                              <Grid item xs>
+                                <Typography gutterBottom variant='subtitle1'>
+                                  <p className='titulo'>{item.menu}</p>
+                                </Typography>
+                                <Typography variant='body2' gutterBottom>
+                                  <div className='detail'> Detalle:</div>
+                                  <p className='parrafo'>{item.details}</p>
+
+                                </Typography>
+                                <Typography variant='body2' gutterBottom>
+                                  <p className='detail'>Ensalada:</p>
+                                  <p className='parrafo'>{item.salad}</p>
+                                </Typography>
+                                <Typography variant='body2' gutterBottom>
+                                  <p className='detail'>Postre:</p>
+                                  <p className='parrafo'>{item.dessert}</p>
+                                </Typography>
+                                <Typography variant='body2' gutterBottom>
+                                  <p className='detail'>Jugo o Bebida:</p>
+                                  <p className='parrafo'>{item.drink}</p>
+                                </Typography>
+                              </Grid>
+                            </Grid>
+                            <Grid item>
+                              <Typography variant='subtitle1'><h2 className='precio'>$ {item.price}</h2></Typography>
+                            </Grid>
+                          </Grid>
+                        </Grid>
+                      </Paper>
+                    </Grid>
+                  </Grid>
+                </TabPanel>
+              ))
+            }
+          </div>
+
+        </div>
         
-        <Tab label="Menu 1" {...a11yProps(0)} />
-        <Tab label="Menu 2" {...a11yProps(1)} />
-        <Tab label="Menu 3" {...a11yProps(2)} />
-        <Tab label="Menu 4" {...a11yProps(3)} />
-        <Tab label="Menu 5" {...a11yProps(4)} />
-
-      </Tabs>
-      <div className="tab1">
-      <TabPanel  value={value} index={0}>
-      <Grid  container spacing={3}>
-        <Grid  item xs={6}>
-          <Paper  className={selectionop.paper}>
-          <Grid container spacing={2}>
-          <Grid item>
-              
-            <ButtonBase className={selectionop.image}>
-              <img className={selectionop.img} alt="complex" src="images/especias.jpg" />
-            </ButtonBase>
-          </Grid>
-          <Grid item xs={12} sm container>
-            <Grid item xs container direction="column" spacing={2}>
-              <Grid item xs>
-                <Typography gutterBottom variant="subtitle1">
-               <p className="titulo"> CAZUELA DE POLLO</p>
-                </Typography>
-                <Typography variant="body2" gutterBottom>
-                 <div className="detail"> Detalle:</div> 
-                 <p className="parrafo"> Elaborado con una presa de carne de vacuno,</p>
-                 <p className="parrafo"> de pollo, de pavo, de ganso o de gallina de </p>
-                 <p className="parrafo"> campo, más verduras variadas: zapallo, choclo,papa.</p>
-                   
-                </Typography>
-                <Typography variant="body2" gutterBottom>
-                 <p className="detail">Ensalada:</p>  
-                 <p className="parrafo">Lechuga</p>
-                </Typography>
-                <Typography variant="body2" gutterBottom>
-                <p className="detail">Postre:</p>  
-                 <p className="parrafo">Helado</p>
-                </Typography>
-                <Typography variant="body2" gutterBottom>
-                <p className="detail">Jugo o Bebida:</p>  
-                 <p className="parrafo">Coca Cola</p>
-                </Typography>
-              
-              </Grid>
-              
-            </Grid>
-            <Grid item>
-              <Typography variant="subtitle1"><h2 className="precio">$5000</h2></Typography>
-            </Grid>
-          </Grid>
-        </Grid>
-          </Paper>
-        </Grid>    
-        </Grid>
-      </TabPanel>
-      <TabPanel  value={value} index={1}>
-
- 
-        <Grid  container spacing={3}>
-        <Grid  item xs={6}>
-          <Paper  className={selectionop.paper}>
-          <Grid container spacing={2}>
-          <Grid item>
-              
-            <ButtonBase className={selectionop.image}>
-              <img className={selectionop.img} alt="complex" src="images/especias.jpg" />
-            </ButtonBase>
-          </Grid>
-          <Grid item xs={12} sm container>
-            <Grid item xs container direction="column" spacing={2}>
-              <Grid item xs>
-                <Typography gutterBottom variant="subtitle1">
-               <p className="titulo"> TALLARINES CON POLLO</p>
-                </Typography>
-                <Typography variant="body2" gutterBottom>
-                 <div className="detail"> Detalle:</div> 
-                 <p className="parrafo"> Elaborado con una presa de pollo,</p>
-                 <p className="parrafo"> acompañado de fideos y salsa de tomate</p>
-            
-                   
-                </Typography>
-                <Typography variant="body2" gutterBottom>
-                 <p className="detail">Ensalada:</p>  
-                 <p className="parrafo">Apio con limon</p>
-                </Typography>
-                <Typography variant="body2" gutterBottom>
-                <p className="detail">Postre:</p>  
-                 <p className="parrafo">Arroz con leche</p>
-                </Typography>
-                <Typography variant="body2" gutterBottom>
-                <p className="detail">Jugo o Bebida:</p>  
-                 <p className="parrafo">Jugo de naranja</p>
-                </Typography>
-              
-              </Grid>
-              
-            </Grid>
-            <Grid item>
-              <Typography variant="subtitle1"><h2 className="precio">$5000</h2></Typography>
-            </Grid>
-          </Grid>
-        </Grid>
-          </Paper>
-        </Grid>    
-        </Grid>
-     
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-      <h1>Menu 3</h1> 
-        <h2>DETALLE</h2>
-        <h3>Imagen</h3>  
-      </TabPanel>
-      <TabPanel value={value} index={3}>
-      <h1>Menu 4</h1> 
-        <h2>DETALLE</h2>
-        <h3>Imagen</h3>  
-      </TabPanel>
-      <TabPanel value={value} index={4}>
-      <h1>Menu 5</h1> 
-        <h2>DETALLE</h2>
-        <h3>Imagen</h3>  
-      </TabPanel>
       </div>
-      
-    </div>
-    <Button
-    role="link"
-    href="selection"
-       variant="contained"
-       color="primary"
-       className={menu.button}
-       startIcon={<PlaylistAddCheckIcon />}
-     >
-       Seleccionar Menú
-     </Button>
-     <Button
-     role="link"
-     href="assemble"
-       variant="contained"
-       color="secondary"
-       className={menu.button}
-       
-     >
-       Armar Menú
-       <PlaylistAddCheckIcon className="icon" />
-     </Button>
-    </div>
-     
-     
-   
-   </>
+      <div className={selectionop.contentFood}>
+          <Button
+            role='link'
+            href='selection'
+            variant='contained'
+            color='primary'
+            startIcon={<PlaylistAddCheckIcon />}
+          >
+            Seleccionar Menú
+          </Button>
+          <Button
+            role='link'
+            href='assemble'
+            variant='contained'
+            color='secondary'
+          >
+            Armar Menú
+            <PlaylistAddCheckIcon className='icon' />
+          </Button>
+        </div>
+    </>
   );
 }
 
 
 
+export default VerticalTabs;
--- src/containers/SelectionMenu/SelectionMenuStyle.js
+++ src/containers/SelectionMenu/SelectionMenuStyle.js
@@ -3,81 +3,83 @@ import InputBase from '@material-ui/core/InputBase';
 
 
 export const SelectionTb = withStyles((theme) => ({
-    root: {
-      'label + &': {
-        marginTop: theme.spacing(3),
-      },
+  root: {
+    'label + &': {
+      marginTop: theme.spacing(3),
     },
-    input: {
+  },
+  input: {
+    borderRadius: 4,
+    position: 'relative',
+    backgroundColor: theme.palette.background.paper,
+    border: '1px solid #ced4da',
+    fontSize: 16,
+    padding: '10px 26px 10px 12px',
+    transition: theme.transitions.create(['border-color', 'box-shadow']),
+    // Use the system font instead of the default Roboto font.
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+    '&:focus': {
       borderRadius: 4,
-      position: 'relative',
-      backgroundColor: theme.palette.background.paper,
-      border: '1px solid #ced4da',
-      fontSize: 16,
-      padding: '10px 26px 10px 12px',
-      transition: theme.transitions.create(['border-color', 'box-shadow']),
-      // Use the system font instead of the default Roboto font.
-      fontFamily: [
-        '-apple-system',
-        'BlinkMacSystemFont',
-        '"Segoe UI"',
-        'Roboto',
-        '"Helvetica Neue"',
-        'Arial',
-        'sans-serif',
-        '"Apple Color Emoji"',
-        '"Segoe UI Emoji"',
-        '"Segoe UI Symbol"',
-      ].join(','),
-      '&:focus': {
-        borderRadius: 4,
-        borderColor: '#80bdff',
-        boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
-      },
+      borderColor: '#80bdff',
+      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
     },
-  }))(InputBase);
+  },
+}))(InputBase);
 
-  export const SelectionOption = makeStyles((theme) => ({
-    root: {
-      flexGrow: 1,
-      backgroundColor: theme.palette.background.paper,
-      display: 'flex',
-      height: 224,
-      width:'auto'
-  
-    
-    },
-    tabs: {
-      borderRight: `1px solid ${theme.palette.divider}`,
-    },
-    paper: {
-      padding: theme.spacing(2),
-      margin: 'auto',
-      height:'auto',
-      
-      width:750,
-    },
-    margin: {
-      margin: theme.spacing(1),
-    },
-    image: {
-      width: 250,
-      height: 300,
-  
-    },
-    img: {
-      margin: 'auto',
-      display: 'block',
-      maxWidth: '100%',
-      maxHeight: '100%',
-    },
-    
-  }));
-  export const menuStyles = makeStyles((theme) => ({
-    button: {
-      margin: theme.spacing(1),
-      marginLeft:630,
-      marginTop:250
-    },
-  }));
-  
+export const SelectionOption = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+    display: 'flex',
+    width: 'auto'
+  },
+  contentFood: {
+    display: 'flex',
+    justifyContent: 'space-evenly',
+    padding:'20px'
+  },
+  tabs: {
+    // borderRight: `2px solid ${theme.palette.divider}`,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    margin: 'auto',
+    height: 'auto',
+
+    // width: 750,
+  },
+  margin: {
+    margin: theme.spacing(1),
+  },
+  image: {
+    width: 250,
+    height: 300,
+
+  },
+  img: {
+    margin: 'auto',
+    display: 'block',
+    maxWidth: '100%',
+    maxHeight: '100%',
+  },
+
+}));
+
+export const menuStyles = makeStyles((theme) => ({
+  button: {
+    margin: theme.spacing(1),
+    marginLeft: 630,
+    marginTop: 250
+  },
+}));
