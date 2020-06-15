a40eb007ff647fee1767c0023ef7c9f842b0f900
alivane <vaneg643@gmail.com>
2020-06-14 23:39:26 -0400

    change data of menu to other files

---
+++ src/data/dataMenu.js
@@ -0,0 +1,21 @@
+
+export const dataMenu = [
+  {
+    menu: 'Menu 1',
+    image: 'images/especias.jpg',
+    price: '5000',
+    drink: 'bebida',
+    dessert: 'Helado',
+    details: 'Detalles',
+    salad: 'Ensalada'
+  },
+  {
+    menu: 'Menu 2',
+    image: 'images/especias.jpg',
+    price: '5000',
+    drink: 'bebida',
+    dessert: 'Helado',
+    details: 'Detalles',
+    salad: 'Ensalada'
+  },
+]
---
+++ src/data/initialData.js
@@ -0,0 +1,13 @@
+export const initialItemMenu = {
+  id: '',
+  menu: "",
+  detail: "",
+  salad: '',
+  dessert: '',
+  img: '',
+  drink: ""
+}
+
+export const dataPriceUnique = {
+  price: '3000'
+}
